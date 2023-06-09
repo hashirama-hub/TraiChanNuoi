14c177c4e08d146581e1c9d1de403901c671d552
hashirama-hub <81902717+hashirama-hub@users.noreply.github.com>
2023-06-09 21:03:49 +0700

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+import pyspark.sql.functions as func
+
+
+# In[2]:
+
+
+spark = SparkSession.builder.appName("LV").getOrCreate()
+
+
+# In[3]:
+
+
+data = spark.read.format("csv").option("header","true").option("inferSchema","true").option("path","hdfs:///user/maria_dev/pokemon/pokemon_data.csv").load()
+
+
+# In[4]:
+
+
+data.show()
+
+
+# In[5]:
+
+
+data.printSchema()
+
+
+# In[8]:
+
+
+data_1 = data.select("#", "Name", "Type 1","HP").withColumn("Time", func.current_timestamp()).cache()
+
+
+# In[10]:
+
+
+data_1.createOrReplaceTempView("datas")
+
+
+# In[11]:
+
+
+spark.sql("SELECT * FROM datas WHERE HP > 20 ORDER BY HP").show()
+
+
+# In[ ]:
+
+
+data_1.write.format("json").mode("overwrite").option("path","hdfs:///user/maria_dev/pokemon/ketqua/").partitionBy("Time").save()
+
